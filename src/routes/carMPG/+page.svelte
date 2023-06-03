38d067cb7ab775d155f566f248770b9dfab11ef8
lsakunes <senukasl@outlook.com>
2023-06-03 13:01:25 -0700

    wow mpgs exist now

---
+++ src/routes/carMPG/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let model = 'ford';
+	let year = '2019';
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<main>
+	<h1>Make API Call</h1>
+
+	<div>
+		<label for="model">Model:</label>
+		<input type="text" id="model" bind:value={model} />
+	</div>
+
+	<div>
+		<label for="year">Year:</label>
+		<input type="text" id="year" bind:value={year} />
+	</div>
+
+	<button
+		on:click={() => {
+			data = {
+				model,
+				year
+			};
+		}}
+	/>
+</main>
+
+<style>
+	main {
+		padding: 20px;
+	}
+
+	label {
+		margin-bottom: 5px;
+		display: block;
+	}
+
+	input {
+		margin-bottom: 10px;
+		padding: 5px;
+		width: 200px;
+	}
+
+	button {
+		padding: 10px 20px;
+		background-color: #007bff;
+		color: white;
+		border: none;
+		cursor: pointer;
+	}
+</style>
