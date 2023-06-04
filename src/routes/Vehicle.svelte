f2595c92d140933c18f535923c79a1f2ba13ab0c
lsakunes <senukasl@outlook.com>
2023-06-03 18:25:01 -0700

    did a thing to select car

--- src/routes/Vehicle.svelte
+++ src/routes/Vehicle.svelte
@@ -1,1 +1,54 @@
-<h1>hi</h1>
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let year = '2012';
+    let make = 'Honda';
+    let model = 'Accord';
+
+    let makes = [] as string[];
+
+    function getMakes() {
+        fetch(`/carMPG/${year}`)
+            .then((res) => res.json())
+            .then((data) => {
+                makes = data;
+        });
+        console.log(makes)
+        make = 'Honda';
+        getModels();
+    }
+
+    let models = [] as string[];
+    function getModels() {
+        fetch(`/carMPG/${year}-${make}`)
+            .then((res) => res.json())
+            .then((data) => {
+                models = data;
+        });
+        model = 'Accord';
+        console.log(models)
+    }
+    onMount(() => {
+        getMakes();
+    });
+</script>
+<h2>Choose Car:</h2>
+<label for="year">Year:</label>
+<input type="number" id="year" min="1984" max="2024" on:change={getMakes} bind:value={year} />
+<br/>
+<label for="make">Make:</label>
+<select id="make" bind:value={make} on:change={getModels}>
+    <option value="" disabled selected>Choose Make</option>
+    {#each makes as m}
+        <option value={m}>{m}</option>
+    {/each}
+</select>
+<br/>
+<label for="model">Model:</label>
+<select id="model" bind:value={model}>
+    <option value="" disabled selected>Choose Model</option>
+    {#each models as m}
+        <option value={m}>{m}</option>
+    {/each}
+</select>
+
--- src/routes/carMPG/[year]/+server.ts
+++ src/routes/carMPG/[year]/+server.ts
@@ -9,29 +9,5 @@ export const GET = (async ({ fetch, params }) => {
 		},
 	});
 	const item = (await res.json()) as { menuItem: { text: string; value: string }[] };
-
-	const makes = item.menuItem.map((item) => item.text);
-	const results = makes.map(async (make) => {
-		const makeId = item.menuItem.find((item) => item.text === make)?.value ?? '';
-		const modelRes = await fetch(
-			`https://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=${params.year}&make=${makeId}`,
-			{
-				headers: {
-					Accept: 'application/json',
-				},
-			}
-		);
-		const modelData = (await modelRes.json()) as {
-			menuItem: { text: string; value: string }[] | { text: string; value: string };
-		};
-
-		const menuItem = modelData.menuItem;
-		if (Array.isArray(menuItem)) {
-			const models = menuItem.map((item) => item.text);
-			return { make, models: models };
-		} else {
-			return { make, models: [menuItem.text] };
-		}
-	});
-	return json(await Promise.all(results));
+	return json(item.menuItem.map((item) => item.text));
 }) as RequestHandler;
