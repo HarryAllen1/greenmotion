bde2c4dbce03dcfacc3e8091d5a935f1fbcc6c91
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-06-03 17:08:54 -0700

    format

--- src/routes/Route.svelte
+++ src/routes/Route.svelte
@@ -1,9 +1,17 @@
 <script>
 	import { Input } from '$components/ui/input';
 	import { onMount } from 'svelte';
+	import { map } from './map';
+	import { get } from 'svelte/store';
 
 	let startLocation = '';
+	let endLocation = '';
+
 	onMount(() => {
+		const directionsService = new google.maps.DirectionsService();
+		const directionsRenderer = new google.maps.DirectionsRenderer();
+		const placesService = new google.maps.places.PlacesService(get(map));
+
 		navigator.geolocation.getCurrentPosition(
 			(pos) => {
 				startLocation = `${pos.coords.latitude}, ${pos.coords.longitude}`;
@@ -16,7 +24,8 @@
 	});
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col gap-4">
 	<h2 class="text-center w-full">Trip Details</h2>
 	<Input bind:value={startLocation} placeholder="Start location" />
+	<Input bind:value={endLocation} placeholder="End location" />
 </div>
--- src/routes/Stats.svelte
+++ src/routes/Stats.svelte
@@ -1,57 +1,56 @@
 <script lang="ts">
-    let vehicleMpg = 22;
-    let biking = true;
-    //0-7 for the ranges
-    let weight = 2;
+	let vehicleMpg = 22;
+	let biking = true;
+	//0-7 for the ranges
+	let weight = 2;
 
-    //kilometers
-    let vehicleDistance = 4;
-    //minutes
-    let vehicleTime = 14;
+	//kilometers
+	let vehicleDistance = 4;
+	//minutes
+	let vehicleTime = 14;
 
-    let pedestrianDistance = 3.4;
-    let pedestrianTime = 72;
+	let pedestrianDistance = 3.4;
+	let pedestrianTime = 72;
 
-    const pedestrianCalories = biking ? ((40+5*weight)+(60+10*weight))/2 : 55 + weight*5;
-    
-    let gallons = vehicleDistance / vehicleMpg;
-    let emissions = 0;
-    let carCalories = 0;
-    
-    let wastedCalories = carCalories - pedestrianCalories;
+	const pedestrianCalories = biking ? (40 + 5 * weight + (60 + 10 * weight)) / 2 : 55 + weight * 5;
 
+	let gallons = vehicleDistance / vehicleMpg;
+	let emissions = 0;
+	let carCalories = 0;
+
+	let wastedCalories = carCalories - pedestrianCalories;
 </script>
+
 <h1>STATISTICS</h1>
 <div>
-    <label for="vehicleMpg">Vehicle MPG:</label>
-    <p>{vehicleMpg}</p>
+	<label for="vehicleMpg">Vehicle MPG:</label>
+	<p>{vehicleMpg}</p>
 </div>
 <div>
-    <label for="biking">Biking:</label>
-    <p>{biking}</p>
+	<label for="biking">Biking:</label>
+	<p>{biking}</p>
 </div>
 <div>
-    <label for="weight">Weight Range:</label>
-    <p>{weight}</p>
+	<label for="weight">Weight Range:</label>
+	<p>{weight}</p>
 </div>
 
 <div>
-    <label for="vehicleDistance">Distance by Car (km):</label>
-    <p>{vehicleDistance}</p>
+	<label for="vehicleDistance">Distance by Car (km):</label>
+	<p>{vehicleDistance}</p>
 </div>
 <div>
-    <label for="vehicleTime">Time by Car (min):</label>
-    <p>{vehicleTime}</p>
+	<label for="vehicleTime">Time by Car (min):</label>
+	<p>{vehicleTime}</p>
 </div>
 <div>
-    <label for="pedestrianDistance">Distance by {biking ? 'Bike' : 'Foot'} (km):</label>
-    <p>{pedestrianDistance}</p>
+	<label for="pedestrianDistance">Distance by {biking ? 'Bike' : 'Foot'} (km):</label>
+	<p>{pedestrianDistance}</p>
 </div>
 <div>
-    <label for="pedestrianTime">Time by {biking ? 'Bike' : 'Foot'} (min):</label>
-    <p>{pedestrianTime}</p>
+	<label for="pedestrianTime">Time by {biking ? 'Bike' : 'Foot'} (min):</label>
+	<p>{pedestrianTime}</p>
 </div>
 <div>
-    <label for="pedestrianCalories">Calories Burned by {biking ? 'Bike' : 'Foot'} (min):</label>
-
-
+	<label for="pedestrianCalories">Calories Burned by {biking ? 'Bike' : 'Foot'} (min):</label>
+</div>
